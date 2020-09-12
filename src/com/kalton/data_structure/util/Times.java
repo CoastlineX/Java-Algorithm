b9cbcd48b754ceeee694f1d1bb2a3f78806fc108
kaltons <1907843600@qq.com>
2020-09-12 21:39:06 +0800

    feat: 完成了链表结构中单向链表的设计
    1. 编写单向链表LinkedList
    2. 编写了带虚拟头结点的Virtual_LinkedList
    3.创建了util包，编写了运行时间测试类，List接口实现类测试类
    4. 编写了链表类测试方法Main，测试动态数组，单向链表，虚拟头结点链表

---
+++ src/com/kalton/data_structure/util/Times.java
@@ -0,0 +1,32 @@
+package com.kalton.data_structure.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * TODO
+ * 时间测试工具类
+ * @author MJ
+ * @date 2020/8/31 - 15:39
+ */
+public class Times {
+	private static final SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+	
+	public interface Task {
+		void execute();
+	}
+	
+	public static void test(String title, Task task) {
+		if (task == null) return;
+		title = (title == null) ? "" : ("【" + title + "】");
+		System.out.println(title);
+		System.out.println("开始：" + fmt.format(new Date()));
+		long begin = System.currentTimeMillis();
+		task.execute();
+		long end = System.currentTimeMillis();
+		System.out.println("结束：" + fmt.format(new Date()));
+		double delta = (end - begin) / 1000.0;
+		System.out.println("耗时：" + delta + "秒");
+		System.out.println("-------------------------------------");
+	}
+}
