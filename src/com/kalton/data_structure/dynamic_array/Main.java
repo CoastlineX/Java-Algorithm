6ac4ef02e1e7d884264e622448441afb66d3f3da
kaltons <1907843600@qq.com>
2020-09-15 13:02:12 +0800

    fix: 修复了初始化动态数组时，允许传入参数为负数的情况
    1. 在DynamicArray带参构造函数中，添加了判断逻辑
    2. 在Main中进行了补充测试

--- src/com/kalton/data_structure/dynamic_array/Main.java
+++ src/com/kalton/data_structure/dynamic_array/Main.java
@@ -9,11 +9,23 @@ package com.kalton.data_structure.dynamic_array;
 public class Main {
 
     public static void main(String[] args) {
+        //常规测试
         DynamicArray<Integer> ints  = new DynamicArray<>();
         ints.add(10);
         ints.add(10);
         ints.add(22);
         ints.add(33);
         System.out.println(ints);
+
+//        //修复初始化数组长度为负数的情况 —— 测试
+//        try {
+//            DynamicArray<Integer> arr = new DynamicArray<>(-1);
+//            arr.add(10);
+//            arr.add(20);
+//            System.out.println(arr);
+//        }catch (IllegalArgumentException e){
+//            e.printStackTrace();
+//            System.out.println(e.getMessage());
+//        }
     }
 }
